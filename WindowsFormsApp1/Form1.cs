d50840d57f6c1ea8dbd22383e51154ca227ca55d
Yoshio Mori <yoshio.mori@hotmail.com>
2018-06-26 00:17:38 -0300

    Etapa 8 https://msdn.microsoft.com/pt-br/library/dd492131.aspx

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,10 @@ namespace WindowsFormsApp1
 
         private void showButton_Click(object sender, EventArgs e)
         {
-
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
         }
 
         private void clearButton_Click(object sender, EventArgs e)
